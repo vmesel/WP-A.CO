a5c814b942bc6525cb5be4ceb5131fd240227b2f
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-03-01 18:02:40 -0300

    Sending things to DB - needs improvement
    Sending things to DB - needs improvement

--- wp-a/wpa.py
+++ wp-a/wpa.py
@@ -3,7 +3,6 @@ WP-A.CO CODE
 BY: VINICIUS MESEL (@VMESEL)
 DISTRIBUTED UNDER THE CC SHARE ALIKE LICENSE(AVAILABLE ON THE LICENSE.md FILE)
 
-- CREATE A CRYPTO HASH FOR EVERY URL
 - WORK ON THE DB AND THE CONNECTION
 """
 ###################  IMPORT LIBRARIES  ##########################
@@ -11,34 +10,42 @@ from flask import Flask,render_template,redirect,request
 import random
 import datetime
 from hashids import Hashids
+import sqlite3 as sql
+from variables import *
 
 cypher = Hashids(salt = "WP-A.co")
-
 app = Flask(__name__)
-
 t = datetime.datetime.now()
 
 def URLGenerate(customaadd, urlprocessar):
 	#This is going to need an improvement for grapping in the database and seeing if any url can be reused
-
 	pi = 3.14159
 	if(customaadd != ""):
-		return("Custom:  " + str(customaadd))
+		return("0")
 	else:
+		return("1")
+
+def processaURL(urlprocessar, customaadd):
+	checker = URLGenerate(urlprocessar, customaadd)
+	if checker == 0:
 		generator = len(urlprocessar) / pi
 		untilNow = (t - datetime.datetime(1970,1,1)).total_seconds()
 		# To generate the Final Hash
 		urlFinal = cypher.encode(int(generator),int(untilNow))
-		return("Sem custom " + str(urlFinal))
 
-def processaURL(urlprocessar, customaadd):
+	else:
+		urlFinal = customaadd
 
-	NewURL = URLGenerate(customaadd, urlprocessar)
-	# Join it to the database
-	# Structure:
-	# ID, URL, ShortHash, Date, Clicks
-
-	
+	# TABLE ORDER: HASH, URL, DATE
+	#insertContent = "'"urlFinal"','" urlprocessar"','" datetime.datetime.now()
+	#insertContent = ("('" + urlFinal "','" + urlprocessar + "'," + datetime.now() ")")
+	connection = sql.connect(DBSource)
+	cursor = connection.cursor()
+	query = "INSERT INTO URLStorage VALUES('{0}','{1}','{2}')".format(urlFinal, urlprocessar, datetime.datetime.now())
+	#return(query)
+	cursor.execute(query)
+	CompleteURL = "Your url is: http://{0}{1}{2}".format(BSUrl, BSFolder, urlFinal)
+	return(CompleteURL)
 
 
 #Home Function
@@ -57,4 +64,4 @@ def addURL():
 
 
 if __name__ == "__main__":
-	app.run(port=8080)
+	app.run(port=8080,debug=True)
