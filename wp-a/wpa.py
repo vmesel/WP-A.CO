a5a6d5085766daee8b74012a5a34453a76ff55f4
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-03-12 12:51:18 -0300

    :smiley: - Indented Code!

--- wp-a/wpa.py
+++ wp-a/wpa.py
@@ -13,88 +13,88 @@ t = datetime.datetime.now()
 SECRAND = SystemRandom()
 
 def processaURL(urlprocessar, customaadd):
+	connection = sql.connect(DBSource)
+	cursor = connection.cursor()
+	CompleteURL = ""
 
- connection = sql.connect(DBSource)
- cursor = connection.cursor()
+	# Check if there is the HTTP:// in the string
+	if "http://" in urlprocessar:
+		urlprocessar = urlprocessar
+	elif "https://" in urlprocessar:
+		urlprocessar = urlprocessar
+	else:
+		urlprocessar = "http://" + urlprocessar
 
- # Check if there is the HTTP:// in the string
- if "http://" in urlprocessar:
-  urlprocessar = urlprocessar
- elif "https://" in urlprocessar:
-  urlprocessar = urlprocessar
- else:
-  urlprocessar = "http://" + urlprocessar
-
- if customaadd == "":
-  key = SECRAND.randint(0, 66 ** 4)
-  urlFinal = safeurl.num_encode(key)
- else:
-  urlFinal = customaadd
+	if customaadd == "":
+		key = SECRAND.randint(0, 66 ** 4)
+		urlFinal = safeurl.num_encode(key)
+	else:
+		urlFinal = customaadd
 
 
- querySelect = "SELECT * FROM URLManager WHERE URL = '{0}'".format(urlprocessar)
+	querySelect = "SELECT * FROM URLManager WHERE URL = '{0}'".format(urlprocessar)
 
- cursor.execute(querySelect)
- if len(cursor.fetchall()) < 1:
-  query = "SELECT case HASH when HASH IS NULL THEN '0' else '1' end from URLManager where HASH = '{0}'".format(urlFinal)
-  cursorNew = connection.cursor()
-  cursorNew.execute(query)
-  sqlquery = str(cursorNew.fetchone()).replace("[", "").replace("]", "")
-  if sqlquery == "None":
-   query2 = "INSERT INTO URLManager(HASH, URL, DATE) VALUES('{0}','{1}','{2}')".format(urlFinal, urlprocessar, datetime.datetime.now())
-   cursorNew.execute(query2)
-  else:
-   return("Error: This hash already exists!")
-  CompleteURL = "http://{0}{1}u/{2}".format(BSUrl, BSFolder, urlFinal)
- else:
-  querySelectHASH = "SELECT HASH FROM URLManager WHERE URL = '{0}'".format(urlprocessar)
-  cursor.execute(querySelectHASH)
-  cursorURL = cursor.fetchone()[0]
-  CompleteURL = "http://{0}{1}u/{2}".format(BSUrl, BSFolder, cursorURL)
+	cursor.execute(querySelect)
+	if len(cursor.fetchall()) < 1:
+		query = "SELECT case HASH when HASH IS NULL THEN '0' else '1' end from URLManager where HASH = '{0}'".format(urlFinal)
+		cursorNew = connection.cursor()
+		cursorNew.execute(query)
+		sqlquery = str(cursorNew.fetchone()).replace("[", "").replace("]", "")
+		if sqlquery == "None":
+			query2 = "INSERT INTO URLManager(HASH, URL, DATE) VALUES('{0}','{1}','{2}')".format(urlFinal, urlprocessar, datetime.datetime.now())
+			cursorNew.execute(query2)
+		else:
+			return("Error: This hash already exists!")
+			CompleteURL = "http://{0}{1}u/{2}".format(BSUrl, BSFolder, urlFinal)
+	else:
+		querySelectHASH = "SELECT HASH FROM URLManager WHERE URL = '{0}'".format(urlprocessar)
+		cursor.execute(querySelectHASH)
+		cursorURL = cursor.fetchone()[0]
+		CompleteURL = "http://{0}{1}u/{2}".format(BSUrl, BSFolder, cursorURL)
 
- connection.commit()
- return(CompleteURL)
+	connection.commit()
+	return(render_template("added.html",FullURL = CompleteURL))
 
 
 #Home Function
 @app.route("/", methods=['GET', 'POST'])
 def home():
- return render_template("index.html")
+	return render_template("index.html")
 
 @app.route("/new/", methods=['GET', 'POST'])
 def newurl():
- return render_template("new.html")
+	return render_template("new.html")
 
 @app.route("/about/", methods=['GET', 'POST'])
 def about():
- return render_template("about.html")
+	return render_template("about.html")
 
 
 @app.route("/add/", methods=['GET', 'POST'])
 def addURL():
- global URLadd, CustomURL
- URLadd = request.args.get('url')
- CustomURL = request.args.get('customshort')
- return(render_template("added.html",FullURL = processaURL(URLadd, CustomURL)))
+	global URLadd, CustomURL
+	URLadd = request.args.get('url')
+	CustomURL = request.args.get('customshort')
+	return(processaURL(URLadd, CustomURL))
 
 @app.route("/api/", methods=['GET', 'POST'])
 def apiURL():
-  global URLadd, CustomURL
-  URLadd = request.args.get('url')
-  CustomURL = request.args.get('customshort')
+	global URLadd, CustomURL
+	URLadd = request.args.get('url')
+	CustomURL = request.args.get('customshort')
 
-  return(processaURL(URLadd, CustomURL))
+	return(processaURL(URLadd, CustomURL))
 
 @app.route("/u/<urlcode>", methods=['GET', 'POST'])
 def CheckURL(urlcode):
- # SELECT URL from URLManager where HASH = urlcode
- connection = sql.connect(DBSource)
- cursor = connection.cursor()
- CheckQuery = "SELECT URL from URLManager where HASH = '{0}'".format(urlcode)
- cursor.execute(CheckQuery)
- connection.commit()
- RedirectTo = cursor.fetchone()[0]
- return render_template("render-url.html",RedirectTo = RedirectTo)
+	# SELECT URL from URLManager where HASH = urlcode
+	connection = sql.connect(DBSource)
+	cursor = connection.cursor()
+	CheckQuery = "SELECT URL from URLManager where HASH = '{0}'".format(urlcode)
+	cursor.execute(CheckQuery)
+	connection.commit()
+	RedirectTo = cursor.fetchone()[0]
+	return render_template("render-url.html",RedirectTo = RedirectTo)
 
 # Server Environment
 #if __name__ == "__main__":
