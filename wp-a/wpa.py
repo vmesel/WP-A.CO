5c3386486830b19a4cce9caa0dad8369e1fceb0d
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-03-16 18:50:06 -0300

    :tada: Corrected Issue #3
    Corrected the Issue #3, creating a new variable

--- wp-a/wpa.py
+++ wp-a/wpa.py
@@ -43,6 +43,7 @@ def processaURL(urlprocessar, customaadd):
 		if sqlquery == "None":
 			query2 = "INSERT INTO URLManager(HASH, URL, DATE) VALUES('{0}','{1}','{2}')".format(urlFinal, urlprocessar, datetime.datetime.now())
 			cursorNew.execute(query2)
+			CompleteURL = "http://{0}{1}u/{2}".format(BSUrl, BSFolder, urlFinal)
 		else:
 			return("Error: This hash already exists!")
 			CompleteURL = "http://{0}{1}u/{2}".format(BSUrl, BSFolder, urlFinal)
@@ -56,7 +57,8 @@ def processaURL(urlprocessar, customaadd):
 	return(CompleteURL)
 
 def NewViewReturn(urlprocessar, customaadd):
-	return(render_template("added.html",FullURL = processaURL(urlprocessar, customaadd)))
+	UrlShortened = processaURL(urlprocessar, customaadd)
+	return render_template("added.html", FullURL = UrlShortened)
 
 
 #Home Function
@@ -96,7 +98,7 @@ def apiURL():
 			return("Error: No Method Defined!")
 	except ValueError:
 		return(ValueError)
-
+		
 @app.route("/u/<urlcode>", methods=['GET', 'POST'])
 def CheckURL(urlcode):
 	# SELECT URL from URLManager where HASH = urlcode
@@ -109,8 +111,8 @@ def CheckURL(urlcode):
 	return render_template("render-url.html",RedirectTo = RedirectTo)
 
 # Server Environment
-#if __name__ == "__main__":
-#app.run(port=BSPort,debug=True,host="0.0.0.0")
-
 if __name__ == "__main__":
-       app.run(port=8080, debug=True)
+ app.run(port=BSPort,debug=True,host="0.0.0.0")
+
+#if __name__ == "__main__":
+#	app.run(port=BSPort)
